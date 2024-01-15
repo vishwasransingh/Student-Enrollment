d24cffd7054eb834665ec2a0c4161563d0589c92
vishwasransing <vishwasransing@gmail.com>
2024-01-15 22:19:00 +0530

    Update index.js
    -Added comments

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,14 +1,20 @@
+// Token for API authentication, edit when using newly generated token.
 var token = "90931827|-31949307100192889|90960648";
+
+// Database name
 var dbName = "SCHOOL-DB";
+// Relationship name (table) in the database
 var relName = "STUDENT-TABLE";
+// Base URL for the database API
 var dbBaseUrl = "http://api.login2explore.com:5577";
 
-var imlEndpoint = "/api/iml";
-var irlEndpoint = "/api/irl";
-var islEndpoint = "/api/isl";
+// API endpoints for different operations
+var imlEndpoint = "/api/iml"; // Index Manipulation Language query endpoint.
+var irlEndpoint = "/api/irl"; // Index Retreival Language query endpoint.
 
+// Function to be executed when the page loads
 function onPageLoad() {
-    // Disable all fields and buttons
+    // Disable all fields and buttons, except roll-number
     document.getElementById('name').disabled = true;
     document.getElementById('class').disabled = true;
     document.getElementById('birthDate').disabled = true;
@@ -17,16 +23,20 @@ function onPageLoad() {
     document.getElementById('saveBtn').disabled = true;
     document.getElementById('updateBtn').disabled = true;
     document.getElementById('resetBtn').disabled = true;
-    // Enable Employee ID field
+    // Enable Roll-Number field and focus on it
     document.getElementById('roll').disabled = false;
     document.getElementById('roll').focus();
 }
 
+// Function to check if a student ID exists in the database
 function checkStudentID() {
     var roll = document.getElementById('roll').value;
 
     if (checkStudentIdInDatabase(roll)) {
+        // If the ID exists, load the data and enable input fields for update
         var record = loadDataFromDatabase(roll);
+        
+        // Populate form fields with the database values
         document.getElementById('roll').value = record.roll;
         document.getElementById('name').value = record.name;
         document.getElementById('class').value = record.class;
@@ -34,18 +44,23 @@ function checkStudentID() {
         document.getElementById('address').value = record.address;
         document.getElementById('enrollmentDate').value = record.enrollmentDate;
 
+        // Disable roll-number field and allow user to edit other fields
         document.getElementById('roll').disabled = true;
         document.getElementById('name').disabled = false;
         document.getElementById('class').disabled = false;
         document.getElementById('birthDate').disabled = false;
         document.getElementById('address').disabled = false;
         document.getElementById('enrollmentDate').disabled = false;
+        
+        // Disable save button, enable update and reset buttons
         document.getElementById('saveBtn').disabled = true;
         document.getElementById('updateBtn').disabled = false;
         document.getElementById('resetBtn').disabled = false;
 
+        // Focus on name field
         document.getElementById('name').focus();
     } else {
+        // If ID doesn't exist, enable input fields for new data entry
         document.getElementById('name').disabled = false;
         document.getElementById('class').disabled = false;
         document.getElementById('birthDate').disabled = false;
@@ -59,6 +74,11 @@ function checkStudentID() {
 
 }
 
+/**
+ * Function to check if a student ID exists in the database
+ * @param {type} id
+ * @returns {Boolean}
+ */
 function checkStudentIdInDatabase(id) {
 
     var jsonRecord = loadDataFromDatabase(id);
@@ -69,15 +89,13 @@ function checkStudentIdInDatabase(id) {
 
 }
 
+// Function to load data from the database based on student ID
 function loadDataFromDatabase(roll) {
-
-    var jsonObj = {
-        roll: roll
-    };
+    var jsonObj = {roll: roll};
     var jsonObjStr = JSON.stringify(jsonObj);
-
     var reqString = createGET_BY_KEYRequest(token, dbName, relName, jsonObjStr, true, true);
 
+    // Synchronous AJAX request to the database
     jQuery.ajaxSetup({async: false});
     var jsonResponseObject = executeCommandAtGivenBaseUrl(reqString, dbBaseUrl, irlEndpoint);
     jQuery.ajaxSetup({async: true});
@@ -90,8 +108,8 @@ function loadDataFromDatabase(roll) {
     return jsonData.record;
 }
 
+// Function to reset the form and disable all fields and buttons except roll-number
 function resetForm() {
-    // Reset all fields and disable all buttons
     document.getElementById('roll').value = '';
     document.getElementById('name').value = '';
     document.getElementById('class').value = '';
@@ -106,11 +124,11 @@ function resetForm() {
     document.getElementById('saveBtn').disabled = true;
     document.getElementById('updateBtn').disabled = true;
     document.getElementById('resetBtn').disabled = true;
-    // Enable Employee ID field
     document.getElementById('roll').disabled = false;
     document.getElementById('roll').focus();
 }
 
+// Function to save data to the database
 function saveData() {
     var jsonObj = getAndValidateFormData();
     
@@ -121,7 +139,8 @@ function saveData() {
     var jsonObjStr = JSON.stringify(jsonObj);
 
     var reqString = createPUTRequest(token, jsonObjStr, dbName, relName);
-
+    
+    // Synchronous AJAX request to the database
     jQuery.ajaxSetup({ async: false });
     var jsonResponseObject = executeCommandAtGivenBaseUrl(reqString, dbBaseUrl, imlEndpoint);
     jQuery.ajaxSetup({ async: true });
@@ -129,8 +148,8 @@ function saveData() {
     resetForm();
 }
 
+// Function to get and validate form data
 function getAndValidateFormData() {
-
     var rollVar = $("#roll").val();
     var nameVar = $("#name").val();
     var classVar = $("#class").val();
@@ -138,6 +157,7 @@ function getAndValidateFormData() {
     var addressVar = $("#address").val();
     var enrollmentDateVar = $("#enrollmentDate").val();
 
+    // Validation for each field
     if (rollVar === "") {
         alert("Roll number can't be blank.");
         $("#roll").focus();
@@ -174,7 +194,7 @@ function getAndValidateFormData() {
         return "";
     }
     
-
+    // Return validated form data as JSON object
     var jsonObj = {
         roll: $("#roll").val(),
         name: $("#name").val(),
@@ -187,12 +207,14 @@ function getAndValidateFormData() {
 
 }
 
+// Function to update data in the database
 function updateData() {
     var jsonObj = getAndValidateFormData();
     var jsonObjStr = JSON.stringify(jsonObj);
     var roll = jsonObj.roll;
     var reqString = createUPDATERecordRequest(token, jsonObjStr, dbName, relName, jsonObj.roll);
     
+    // Synchronous AJAX request to the database
     jQuery.ajaxSetup({ async: false });
     var jsonResponseObject = executeCommandAtGivenBaseUrl(reqString, dbBaseUrl, imlEndpoint);
     jQuery.ajaxSetup({ async: true });
